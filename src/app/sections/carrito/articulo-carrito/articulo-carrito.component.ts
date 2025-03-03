b2180782e5c4331ecdae0fc44ca3b0d01f00f9f4
ASolaDev <antoniosola991@gmail.com>
2025-03-03 18:39:09 +0100

    Añadidos componentes y servicios

---
+++ src/app/sections/carrito/articulo-carrito/articulo-carrito.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-articulo-carrito',
+  imports: [],
+  templateUrl: './articulo-carrito.component.html',
+  styleUrl: './articulo-carrito.component.css'
+})
+export class ArticuloCarritoComponent {
+
+}
